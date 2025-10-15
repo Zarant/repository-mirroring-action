befb27d9c01e421d069bffd20bfec78a6be19de8
Zarant <26726709+Zarant@users.noreply.github.com>
2025-10-15 19:23:57 -0300

    Update mirror.sh

--- mirror.sh
+++ mirror.sh
@@ -3,9 +3,9 @@ set -eu
 
 /setup-ssh.sh
 
-export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no $INPUT_SSH_USERNAME"
+export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
 git remote add mirror "$INPUT_TARGET_REPO_URL"
-git push --tags --force mirror
+git push --tags --prune mirror "refs/remotes/origin/*:refs/heads/*"
 
 # NOTE: Since `post` execution is not supported for local action from './' for now, we need to
 # run the command by hand.
